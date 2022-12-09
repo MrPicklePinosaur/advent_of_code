c0688715ab7c71312bcc82a0d127cc90f2c4785d
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-12-09 03:07:24 -0500

    share data between js and wasm

--- day5/index.js
+++ day5/index.js
@@ -1,19 +1,41 @@
 const fs = require('fs');
+const util = require('util');
+
+const decode = (mem, offset, len) => {
+    const bytes = new Uint8Array(mem.buffer, offset, len);
+    return new util.TextDecoder("utf-8").decode(bytes);
+}
+
+const encode = (mem, offset, str) => {
+    const bytes = new util.TextEncoder().encode(str);
+    const buffer = new Uint8Array(mem.buffer, offset, bytes.byteLength +1);
+    buffer.set(bytes);
+}
 
 // pass i/o handlers to wasm
+const block = new WebAssembly.Memory({initial: 1 });
 const imports = {
     console: {
-	log: (x) => { process.stdout.write(x); },
+	string: (offset, len) => { process.stdout.write(decode(block, offset, len)) },
+	i32: (n) => { process.stdout.write(n+"") }
     },
     fs: {
-	readFileSync(path) => fs.readFileSync(path),
+	readFileSync: (offset, len, outOffset) => {
+	    const file = fs.readFileSync(decode(block, offset, len));
+	    // console.log(encode(block, offset, file));
+	    encode(block, outOffset, file);
+	    return file.length;
+	},
+    },
+    memory: {
+	block: block
     }
 }
 
 const jsMain = async () => {
     const buffer = fs.readFileSync('main.wasm');
     WebAssembly.instantiate(buffer, imports).then(wasm => {
-	wasm.instance.exports.main(10);
+	wasm.instance.exports.main();
     }).catch(e => console.log(e));
 }
 jsMain();
--- day5/main.wat
+++ day5/main.wat
@@ -1,9 +1,25 @@
 (module
-    (import "console" "log" (func $log (param i32)))
-    (import "fs" "readFileSync" (func $readFileSync (param i32)))
+
+    (import "memory" "block" (memory 1))
+    (import "console" "string" (func $logString (param i32 i32)))
+    (import "console" "i32" (func $logI32 (param i32)))
+    (import "fs" "readFileSync" (func $readFileSync (param i32 i32 i32) (result i32)))
     (export "main" (func $main))
 
-    (func $main (param $a i32)
-        (call $log (local.get $a))
+    (data (i32.const 0) "input.txt") ;; constant holding name of input file
+    (data (i32.const 4) "") ;; used for file data
+
+    (func $main (result i32)
+
+    	(local $fileLength i32)
+
+	(local.set $fileLength
+	    (call $readFileSync (i32.const 0) (i32.const 9) (i32.const 4))
+	)
+
+	(call $logI32 (local.get $fileLength))
+	(call $logString (i32.const 4) (i32.const 100))
+
+	(i32.const 0)
     )
 )
